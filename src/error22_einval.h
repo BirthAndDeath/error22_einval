468782410cad9bfd564f85528344b0ca7450d3d3
2301_79117301 <2301_79117301@noreply.gitcode.com>
2025-11-30 11:03:35 +0800

    修改：     src/error22_einval.h 	修改：     src/lib.hpp

--- src/error22_einval.h
+++ src/error22_einval.h
@@ -17,38 +17,6 @@
 #define FFI_PLUGIN_EXPORT
 #endif
 
-#ifdef __ANDROID__
-#include <android/log.h>
-#define LOG_D(fmt, ...)                                                        \
-  __android_log_print(ANDROID_LOG_DEBUG, "error22_einval", "[%s:%d] " fmt,     \
-                      __FILE__, __LINE__, ##__VA_ARGS__)
-#define LOG_I(fmt, ...)                                                        \
-  __android_log_print(ANDROID_LOG_INFO, "error22_einval", "[%s:%d] " fmt,      \
-                      __FILE__, __LINE__, ##__VA_ARGS__)
-#define LOG_W(fmt, ...)                                                        \
-  __android_log_print(ANDROID_LOG_WARN, "error22_einval", "[%s:%d] " fmt,      \
-                      __FILE__, __LINE__, ##__VA_ARGS__)
-#define LOG_E(fmt, ...)                                                        \
-  __android_log_print(ANDROID_LOG_ERROR, "error22_einval", "[%s:%d] " fmt,     \
-                      __FILE__, __LINE__, ##__VA_ARGS__)
-#else
-enum class LogLevel { D, I, W, E };
-template <LogLevel L, typename... Args>
-static inline void log_print(const char *file, int line, const char *fmt,
-                             Args... args) {
-  fprintf(stderr, "[%c %s:%d] ", "DIWE"[static_cast<int>(L)], file, line);
-  fprintf(stderr, fmt, args...);
-  fprintf(stderr, "\n");
-}
-#define LOG_D(fmt, ...)                                                        \
-  log_print<LogLevel::D>(__FILE__, __LINE__, fmt, ##__VA_ARGS__)
-#define LOG_I(fmt, ...)                                                        \
-  log_print<LogLevel::I>(__FILE__, __LINE__, fmt, ##__VA_ARGS__)
-#define LOG_W(fmt, ...)                                                        \
-  log_print<LogLevel::W>(__FILE__, __LINE__, fmt, ##__VA_ARGS__)
-#define LOG_E(fmt, ...)                                                        \
-  log_print<LogLevel::E>(__FILE__, __LINE__, fmt, ##__VA_ARGS__)
-#endif
 // A very short-lived native function.
 //
 // For very short-lived functions, it is fine to call them on the main isolate.
@@ -74,7 +42,6 @@ FFI_PLUGIN_EXPORT void init();
 FFI_PLUGIN_EXPORT void set_log_callback(void (*callback)(const char *message));
 typedef void (*LogCallback)(const char *message);
 
-// 保存回调函数指针
 extern LogCallback g_log_callback;
 //@brief log日志函数
 inline void my_log(const char *message) {
--- src/lib.hpp
+++ src/lib.hpp
@@ -1,4 +1,39 @@
 #pragma once
+#include <stdint.h>
+#include <stdio.h>
+#include <sys/syslog.h>
+#ifdef __ANDROID__
+#include <android/log.h>
+#define LOG_D(fmt, ...)                                                        \
+  __android_log_print(ANDROID_LOG_DEBUG, "error22_einval", "[%s:%d] " fmt,     \
+                      __FILE__, __LINE__, ##__VA_ARGS__)
+#define LOG_I(fmt, ...)                                                        \
+  __android_log_print(ANDROID_LOG_INFO, "error22_einval", "[%s:%d] " fmt,      \
+                      __FILE__, __LINE__, ##__VA_ARGS__)
+#define LOG_W(fmt, ...)                                                        \
+  __android_log_print(ANDROID_LOG_WARN, "error22_einval", "[%s:%d] " fmt,      \
+                      __FILE__, __LINE__, ##__VA_ARGS__)
+#define LOG_E(fmt, ...)                                                        \
+  __android_log_print(ANDROID_LOG_ERROR, "error22_einval", "[%s:%d] " fmt,     \
+                      __FILE__, __LINE__, ##__VA_ARGS__)
+#else
+enum class LogLevel { D, I, W, E };
+template <LogLevel L, typename... Args>
+static inline void log_print(const char *file, int line, const char *fmt,
+                             Args... args) {
+  fprintf(stderr, "[%c %s:%d] ", "DIWE"[static_cast<int>(L)], file, line);
+  fprintf(stderr, fmt, args...);
+  fprintf(stderr, "\n");
+}
+#define LOG_D(fmt, ...)                                                        \
+  log_print<LogLevel::D>(__FILE__, __LINE__, fmt, ##__VA_ARGS__)
+#define LOG_I(fmt, ...)                                                        \
+  log_print<LogLevel::I>(__FILE__, __LINE__, fmt, ##__VA_ARGS__)
+#define LOG_W(fmt, ...)                                                        \
+  log_print<LogLevel::W>(__FILE__, __LINE__, fmt, ##__VA_ARGS__)
+#define LOG_E(fmt, ...)                                                        \
+  log_print<LogLevel::E>(__FILE__, __LINE__, fmt, ##__VA_ARGS__)
+#endif
 
 #include "llama.h"
 
